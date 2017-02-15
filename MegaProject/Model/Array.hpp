b5ec33dec80e1e433ef83bbfb2e6e92d7561bd7c
Tbone288 <tylerodom2@gmail.com>
2017-02-15 13:32:12 -0700

    created array.hpp

---
+++ MegaProject/Model/Array.hpp
@@ -0,0 +1,85 @@
+//
+//  Array.hpp
+//  MegaProject
+//
+//  Created by Odom, Tyler on 2/15/17.
+//  Copyright © 2017 Odom, Tyler. All rights reserved.
+//
+
+#ifndef Array_h
+#define Array_h
+
+#include "Node.hpp"
+#include <assert.h>
+
+template <class Type>
+class Array
+{
+private:
+    int size;
+    Node<Type> * front'
+public:
+    Array();
+    Array(int size);
+    void setAtIndex(int index, Type value);
+    Type getFromIndex(int index);
+    int getSize();
+};
+
+template <class Type>
+Array <Type> :: Array()
+{
+    //Never used
+    // Default constructor only supplied to avoid compilation error!
+}
+
+template <class Type>
+Array <Type> :: Array(int size)
+{
+    assert(size > 0);
+    
+    this->size = size;
+    this ->front = new Node<Type>();
+    
+    for(int index - 1; index < size; index++)
+    {
+        Node <Type> * current = new Node<Type>();
+        current->setNodePointer(front);
+        front = current;
+    }
+    
+}
+
+template <class Type>
+void Array<Type> :: setAtIndex(int index, Type data)
+{
+    assert(index >= 0 && index < size);
+    Node<Type> * current = front;
+    for(int spot = 0; spot < index; spot++)
+}
+
+template <class Type>
+Type Array<Type> :: getFromIndex(int index)
+{
+    assert(index >= 0 && index < size);
+    
+    Type value;
+    
+    Node<Type>* current = front;
+    for(int position = 0; position < index++; position++)
+    {
+        current = current -> getNodePointer();
+    }
+    
+    value = current -> getNodeData();
+    return value;
+}
+
+
+template <class Type>
+int Array<Type> :: getSize()
+{
+    return size;
+}
+
+#endif /* Array_h */
