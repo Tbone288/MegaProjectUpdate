0e4c0d97024d4b61000039f35b561ad275380ddb
Tbone288 <tylerodom2@gmail.com>
2017-02-27 13:52:11 -0700

    Added BiDirectionalNode header.

---
+++ MegaProject/Model/BiDirectionalNode.hpp
@@ -0,0 +1,38 @@
+//
+//  BiDirectionalNode.hpp
+//  MegaProject
+//
+//  Created by Odom, Tyler on 2/27/17.
+//  Copyright © 2017 Odom, Tyler. All rights reserved.
+//
+
+#ifndef BiDirectionalNode_h
+#define BiDirectionalNode_h
+
+template <class Type>
+class BiDirectionalNode : public Node<Type>
+{
+private:
+    BiDirectionalNode<Type> * previous;
+    BiDirectionalNode<Type> * next;
+    Type data;
+public:
+    BiDirectionalNode();
+    BiDirectionalNode(Type data);
+    BiDirectionalNode(Type data, BiDirectionalNode<Type> * previous,
+        BiDirectionalNode<Type> * next);
+}
+
+template <class Type>
+BiDirectionalNode<Type> :: BiDirectionalNode() : Node<Type>()
+{
+    
+}
+
+BiDirectionalNode<Type> :: BiDirectionalNode(Type data) : Node<Type>(data)
+{
+    
+}
+
+
+#endif /* BiDirectionalNode_h */
