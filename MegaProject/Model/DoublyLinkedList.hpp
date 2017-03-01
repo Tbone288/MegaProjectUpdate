99aee8a0b3f96f31cd635131d747519679680401
Tbone288 <tylerodom2@gmail.com>
2017-03-01 12:51:45 -0700

    created doublyLinkedList header.

---
+++ MegaProject/Model/DoublyLinkedList.hpp
@@ -0,0 +1,21 @@
+//
+//  DoublyLinkedList.hpp
+//  MegaProject
+//
+//  Created by Odom, Tyler on 3/1/17.
+//  Copyright © 2017 Odom, Tyler. All rights reserved.
+//
+
+#ifndef DoublyLinkedList_h
+#define DoublyLinkedList_h
+
+#include "BiDirectionalNode.hpp"
+
+template <class Type>
+class DoublyLinkedList
+{
+    
+};
+
+
+#endif /* DoublyLinkedList_h */
